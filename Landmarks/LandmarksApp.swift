3d2524b522cdecf2ad768c7af6552300e71f5198
Austin Suarez <austinmsuarez@gmail.com>
2023-04-16 13:11:40 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Austin Suarez on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
